6ffe9a0ec92f1c7008644a977ed99e593c6887c1
Boris Faure <billiob@gmail.com>
2023-08-25 12:12:12 +0200

    errors: use thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,58 +1,18 @@
+use thiserror::Error;
+
 /// Error enumerates all possible errors returned by this library.
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
     /// Error while parsing XML
-    XMLError(::quick_xml::Error),
+    #[error("Error while parsing XML")]
+    XMLError(#[from] ::quick_xml::Error),
     /// Error while converting f64 attribute
-    ParseFloatError(std::num::ParseFloatError),
+    #[error("Error while converting f64 attribute")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
     /// Error while converting u64 attribute
-    ParseIntError(std::num::ParseIntError),
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Error::XMLError(e) => Some(e),
-            Error::ParseFloatError(e) => Some(e),
-            Error::ParseIntError(e) => Some(e),
-        }
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Error::XMLError(err) => write!(f, "XML error: {}", err),
-            Error::ParseFloatError(err) => write!(f, "ParseFloat error: {}", err),
-            Error::ParseIntError(err) => write!(f, "ParseInt error: {}", err),
-        }
-    }
-}
-
-impl From<::std::num::ParseFloatError> for Error {
-    #[inline]
-    fn from(error: ::std::num::ParseFloatError) -> Error {
-        Error::ParseFloatError(error)
-    }
-}
-
-impl From<::std::num::ParseIntError> for Error {
-    #[inline]
-    fn from(error: ::std::num::ParseIntError) -> Error {
-        Error::ParseIntError(error)
-    }
-}
-
-impl From<::std::str::Utf8Error> for Error {
-    #[inline]
-    fn from(error: ::std::str::Utf8Error) -> Error {
-        Error::XMLError(::quick_xml::Error::Utf8(error))
-    }
-}
-
-impl From<::quick_xml::Error> for Error {
-    #[inline]
-    fn from(err: ::quick_xml::Error) -> Error {
-        Error::XMLError(err)
-    }
+    #[error("Error while converting u64 attribute")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    /// Error while converting bytes to Utf8
+    #[error("Error while converting bytes to Utf8")]
+    ParseUt8Error(#[from] std::str::Utf8Error),
 }
