6fa046520a9e443c5c3799e58482d250d3af80b9
Boris Faure <billiob@gmail.com>
2023-09-22 22:59:05 +0200

    doc: add top-level example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
 #![warn(missing_docs)]
 //! Library to parse JUnit XML files
+//!
+//! # Example
+//!
+//! Parsing a JUnit content
+//!
+//! ```
+//! use std::io::Cursor;
+//!     let xml = r#"
+//! <testsuite tests="3" failures="1">
+//!   <testcase classname="foo1" name="ASuccessfulTest"/>
+//!   <testcase classname="foo2" name="AnotherSuccessfulTest"/>
+//!   <testcase classname="foo3" name="AFailingTest">
+//!     <failure type="NotEnoughFoo"> details about failure </failure>
+//!   </testcase>
+//! </testsuite>
+//! "#;
+//!     let cursor = Cursor::new(xml);
+//!     let r = junit_parser::from_reader(cursor);
+//!     assert!(r.is_ok());
+//!     let t = r.unwrap();
+//!     assert_eq!(t.suites.len(), 1);
+//!     let ts = &t.suites[0];
+//!     assert_eq!(ts.tests, 3);
+//!     assert_eq!(ts.failures, 1);
+//!     assert_eq!(ts.cases.len(), 3);
+//!     assert!(ts.cases[0].status.is_success());
+//!     assert!(ts.cases[2].status.is_failure());
+//! ```
 
 /// Errors
 mod errors;
