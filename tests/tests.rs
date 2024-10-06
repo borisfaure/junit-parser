f2e43b8dcecc7dc961972b2422b77e6fae98772f
Boris Faure <billiob@gmail.com>
2024-10-06 12:06:20 +0200

    tests: test system-{out,err} with CDATA

--- tests/tests.rs
+++ tests/tests.rs
@@ -1039,7 +1039,7 @@ fn test_attr_unescaped() {
 /// Testing parsing system-out/err
 /// Ensure whitespace is kept
 fn test_system_out_err() {
-    let xml = r#"
+    let xml1 = r#"
     <testsuite name="foo" tests="1" time="30.23">
       <testcase name="bar" time="12.34" >
         <system-out>
@@ -1060,7 +1060,104 @@ fn test_system_out_err() {
   ts-err
       </system-err>
     </testsuite>"#;
-    let cursor = Cursor::new(xml);
+    let xml2 = r#"
+    <testsuite name="foo" tests="1" time="30.23">
+      <testcase name="bar" time="12.34" >
+        <system-out><![CDATA[
+  tc-out
+  tc-out
+        ]]></system-out>
+        <system-err><![CDATA[
+  tc-err
+  tc-err
+        ]]></system-err>
+      </testcase>
+      <system-out><![CDATA[
+  ts-out
+  ts-out
+      ]]></system-out>
+      <system-err><![CDATA[
+  ts-err
+  ts-err
+      ]]></system-err>
+    </testsuite>"#;
+    for xml in [xml1, xml2] {
+        let cursor = Cursor::new(xml);
+        let r = junit_parser::from_reader(cursor);
+        assert!(r.is_ok());
+        let t = r.unwrap();
+        assert_eq!(t.suites.len(), 1);
+        let ts = &t.suites[0];
+        assert!(ts.system_out.is_some());
+        assert_eq!(
+            ts.system_out,
+            Some(
+                r#"
+  ts-out
+  ts-out
+      "#
+                .to_string()
+            )
+        );
+        assert!(ts.system_err.is_some());
+        assert_eq!(
+            ts.system_err,
+            Some(
+                r#"
+  ts-err
+  ts-err
+      "#
+                .to_string()
+            )
+        );
+        assert_eq!(ts.cases.len(), 1);
+        let tc = &ts.cases[0];
+        assert!(tc.system_out.is_some());
+        assert!(tc.system_out.is_some());
+        assert_eq!(
+            tc.system_out,
+            Some(
+                r#"
+  tc-out
+  tc-out
+        "#
+                .to_string()
+            )
+        );
+        assert!(tc.system_err.is_some());
+        assert_eq!(
+            tc.system_err,
+            Some(
+                r#"
+  tc-err
+  tc-err
+        "#
+                .to_string()
+            )
+        );
+    }
+    let xml3 = r#"
+    <testsuite name="foo" tests="1" time="30.23">
+      <testcase name="bar" time="12.34" >
+        <system-out><![CDATA[
+  <tc-out>
+  </tc-out>
+        ]]></system-out>
+        <system-err><![CDATA[
+  <tc-err>
+  </tc-err>
+        ]]></system-err>
+      </testcase>
+      <system-out><![CDATA[
+  <ts-out>
+  </ts-out>
+      ]]></system-out>
+      <system-err><![CDATA[
+  <ts-err>
+  </ts-err>
+      ]]></system-err>
+    </testsuite>"#;
+    let cursor = Cursor::new(xml3);
     let r = junit_parser::from_reader(cursor);
     assert!(r.is_ok());
     let t = r.unwrap();
@@ -1071,8 +1168,8 @@ fn test_system_out_err() {
         ts.system_out,
         Some(
             r#"
-  ts-out
-  ts-out
+  <ts-out>
+  </ts-out>
       "#
             .to_string()
         )
@@ -1082,8 +1179,8 @@ fn test_system_out_err() {
         ts.system_err,
         Some(
             r#"
-  ts-err
-  ts-err
+  <ts-err>
+  </ts-err>
       "#
             .to_string()
         )
@@ -1096,8 +1193,8 @@ fn test_system_out_err() {
         tc.system_out,
         Some(
             r#"
-  tc-out
-  tc-out
+  <tc-out>
+  </tc-out>
         "#
             .to_string()
         )
@@ -1107,8 +1204,8 @@ fn test_system_out_err() {
         tc.system_err,
         Some(
             r#"
-  tc-err
-  tc-err
+  <tc-err>
+  </tc-err>
         "#
             .to_string()
         )
